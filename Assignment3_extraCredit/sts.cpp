31a3c11ac8928c60cc6384038ab7a64d5f03ee31
Mike Yannuzzi (University of Houston) <43522450+myannuzzi@users.noreply.github.com>
2022-04-22 19:23:54 -0500

    added more to progam

--- Assignment3_extraCredit/sts.cpp
+++ Assignment3_extraCredit/sts.cpp
@@ -1,11 +1,21 @@
-// sts.cpp
-// C++ file to run sts algorithm
-// Written by Mike Yannuzzi
+//sts.cpp
+//C++ file to run sts algorithm
+//Written by Mike Yannuzzi
 
 #include <iostream>
 
 using namespace std;
 
 int main(){
-    cout << "Hello World!" << endl;
+    cout << "Running sts program..." << endl;
+
+    //Read in data file
+
+    //Run loop for 200 iterations
+    for(int i=0;i<200;i++){
+
+    }
+    cout << "Program finished. Printing out results..." << endl;
+    //Print out results
+    
 }

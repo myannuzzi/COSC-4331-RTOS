fa2de074434d389f0d49b2400d7cd7dcf15c08f9
Mike Yannuzzi (University of Houston) <43522450+myannuzzi@users.noreply.github.com>
2022-04-29 23:23:24 -0500

    Need to fix sts

--- Assignment3_extraCredit/sts.cpp
+++ Assignment3_extraCredit/sts.cpp
@@ -49,27 +49,27 @@ void sts(int time, vector<task> *tasks, priority_queue<task> *runQueue, int *com
     for(int i=0;i<tasks->size();i++){
         if(tasks[i]->a == time){
             cout << "Event 1: a new task has arrived! Running performance test" << endl;
-            // Run the acceptance test
-            int sumRc=0;
-            priority_queue<task> copyQueue = runQueue;
-            for(int j=0;j<copyQueue->size();i++){
-                sumRc = sumRc + copyQueue->top().rc;
-                copyQueue->pop();
-            }
-            double D = tasks[i]->d;
-            double U = double(sumRc) / D;
-            cout << "New U = " << U << endl;
-            if(U>1){
-                cout << "U is greater than 1, task not accepted..." << endl;
-                runQueue->top().rc--;
-                return;
-            }else {
-                cout << "Task accepted, setting voltage and inserting into running queue..." << endl;
-                runQueue->push(tasks[i], tasks[i].d);
-                runQueue->top().rc--;
-                //I am going to keep my tasks in the vector they are in. As long as the time doesnt reverse (which it wont) those tasks wont be seen again
-                return;
-            }
+            // // Run the acceptance test
+            // int sumRc=0;
+            // priority_queue<task> copyQueue = runQueue;
+            // for(int j=0;j<copyQueue->size();i++){
+            //     sumRc = sumRc + copyQueue->top().rc;
+            //     copyQueue->pop();
+            // }
+            // double D = tasks[i]->d;
+            // double U = double(sumRc) / D;
+            // cout << "New U = " << U << endl;
+            // if(U>1){
+            //     cout << "U is greater than 1, task not accepted..." << endl;
+            //     runQueue->top().rc--;
+            //     return;
+            // }else {
+            //     cout << "Task accepted, setting voltage and inserting into running queue..." << endl;
+            //     runQueue->push(tasks[i], tasks[i].d);
+            //     runQueue->top().rc--;
+            //     //I am going to keep my tasks in the vector they are in. As long as the time doesnt reverse (which it wont) those tasks wont be seen again
+            //     return;
+            // }
         }
 
         //Schedule only the tasks in [t, D_k] at the proper voltage and remove tasks from the set
